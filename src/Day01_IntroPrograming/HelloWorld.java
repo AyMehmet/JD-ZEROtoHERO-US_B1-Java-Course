d62a66aae9fa8ab2fa199c916c014a39492e2f7f
aymehmet <aymehmet1976@gmail.com>
2022-08-19 01:08:20 +0300

    Day1-2

--- src/Day01_IntroPrograming/HelloWorld.java
+++ src/Day01_IntroPrograming/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {  // This is class header
     public static void main(String[] args) { // This is main method (makes our java file runnable)
 
         System.out.println("Hello  World");  // It prints "Hello World" on the console
-
+        System.out.println("Hello  World");
     }
 
 }
